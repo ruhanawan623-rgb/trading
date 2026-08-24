d04b7a3d54df4729af97148edaee83a1f56b1766
ruhanawan623-rgb <ruhanawan623@gmail.com>
2026-08-24 15:17:33 +0500

    Upload start_trading.bat

---
+++ start_trading.bat
@@ -0,0 +1,8 @@
+@echo off
+title Olymp Trade Pro
+echo ====================================================
+echo Starting Olymp Trade Pro Web Platform...
+echo ====================================================
+start "" "http://127.0.0.1:3000"
+python server.py
+pause
